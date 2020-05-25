c7ab394dfe06c9a4f7d43c83077a435c6cf6273a
Marat Khusyainov <63118353+MaratKhusyainov@users.noreply.github.com>
2020-05-25 16:13:44 +0300

    Домашняя работа № 4

--- src/Homework/Main.java
+++ src/Homework/Main.java
@@ -1,112 +1,174 @@
 package Homework;
 
-import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    private static final char DOT_EMPTY = '.';
+    private static final char DOT_X = 'X';
+    private static final char DOT_0 = '0';
+    private static int winLength;
 
-    public static void main(String[] args) {
-        /**
-         * * 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-         * С помощью цикла и условия заменить 0 на 1, 1 на 0;
-         */
-        int[] a1 = {0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1};
-        System.out.println("Задание 1");
-        for (int i = 0; i < a1.length; i++) {
-            if (a1[i] == 1) {
-                a1[i] = 0;
-            } else a1[i] = 1;
-            System.out.println(" " + a1[i]);
-        }
-        System.out.println();
+    private static Scanner scanner;
+    private static Random random;
+    private static int mapSize;
+    private static char[][]map;
 
-        /**
-         * 2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
-         */
-        int[] a2 = new int[8];
-        System.out.println("Задание 2");
-        for (int i = 0; i < a2.length; i++) {
-            a2[i] = i * 3;
-            System.out.println(" " + a2[i]);
-        }
-        System.out.println();
-
-        /**
-         * 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
-         */
-        int a3[] = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println("Задание 3");
-        for (int i = 0; i < a3.length; i++) {
-            if (a3[i] < 6) {
-                a3[i] = a3[i] * 2;
-            } else a3[i] = a3[i];
-            System.out.println(" " + a3[i]);
-        }
-        System.out.println();
-
-
-        /**
-         * 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
-         *  и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
-         */
-        int[][] a4 = new int[11][11];
-        System.out.println("Задание 4");
-        for (int i = 0; i < a4.length; i++) {
-            for (int j = 0; j < a4.length; j++) {
-                if (i == j){
-                    a4[i][j] = 1;}
-                else if (i == a4.length -(j+1)) {
-                    a4[i][j] = 1;}
-                else a4 [i][j] = 0;
-                System.out.println(a4[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        /**
-         * 5. Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета)
-         */
-        System.out.println("Задание 5");
-        int[] a5 = {21, 3, 1, 5, 8, 14, 11, 4, 17};
-        int min = a5[0];
-        int max = a5[0];
-        for (int i = 0; i < a5.length; i++) {
-            if (a5[i] > max) {
-                max = a5[i];
-            }
-            if (a5[i] < min) {
-                min = a5[i];
-            }
-        }
-        System.out.println("Maximum: " + max);
-        System.out.println("Minimum: " + min);
-
-        /**
-         *  6. Написать метод, в который передается не пустой одномерный целочисленный массив,
-         *  метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
-         */
-        int[] a6 = {5, 3, 9, 1, 15, 8, 5, 6, 4, 8, 1, 1};
-        System.out.println("Задание 6 ");
-        System.out.println("Результат = " + check(a6));
+    public static void main (String[] args) {
+        game();
     }
-        private static boolean check(int[] a6) {
-        int sumLeft, sumRight;
-        for (int i = 0; i < a6.length + 1; i++) {
-            sumLeft = 0;
-            sumRight = 0;
-            for (int j = 0; j < i; j++) {
-                sumLeft += a6[j];
+
+    private static void game() {
+
+        scanner = new Scanner(System.in);
+        random = new Random();
+
+        System.out.println("Приветствую вас в игре Крестики-Нолики");
+        setMapSize();
+        setWinLenght();
+        initMap();
+
+        System.out.println("Перед вами поле");
+        printMap();
+
+        System.out.println("Вы играете крестиками");
+        System.out.println("Что бы сделать ход, введите номер строки и номер столбца на поле");
+
+        while (true) {
+            if (!nextRound()) break;
+        }
+
+        System.out.println("Новая игра - game, Выход - exit");
+        switch (scanner.next()) {
+            case "game":
+                game();
+            case "exit":
+                return;
+            default:
+        }
+    }
+
+    private static boolean nextRound() {
+
+        humanTurn();
+        printMap();
+        if (checkVictory(DOT_X)) {
+            System.out.println("Вы проиграли");
+            return false;
+        }
+        if (isMapFull()) {
+            System.out.println("Ничья");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void setWinLenght() {
+        do {
+            System.out.println("Введите желаемое количество символов для победы");
+            System.out.println("Значение не может быть больше размера поля и меньше 2");
+            winLength = scanner.nextInt();
+        } while (winLength < 2 || winLength > mapSize);
+    }
+
+    private static void setMapSize() {
+        do {
+            System.out.println("Выберите размер поля от 3 до 10 (X:X):");
+            mapSize = scanner.nextInt();
+        } while (mapSize < 3 || mapSize > 10);
+    }
+
+    public static void initMap() {
+        map = new char[mapSize][mapSize];
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                map[i][j] = DOT_EMPTY;
             }
-            for (int j = i; j < a6.length; j++) {
-                sumRight += a6[j];
+        }
+    }
+
+    public static void printMap() {
+        for (char[] chars : map) {
+            for (char symbol : chars) {
+                System.out.print("|" + symbol);
             }
-            if (sumRight == sumLeft) return true;
+            System.out.println("|");
+        }
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Ваш ход:");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (isCellNotValid(x, y));
+
+        map[y][x] = DOT_X;
+    }
+
+    private static void robotTurn() {
+        int x, y;
+        System.out.println("Ход компьютера:");
+        do {
+            x = random.nextInt(mapSize);
+            y = random.nextInt(mapSize);
+        } while (isCellNotValid(x, y));
+
+        map[y][x] = DOT_0;
+    }
+
+    private static boolean isCellNotValid(int x, int y) {
+        if (x < 0 || x >= mapSize || y < 0 || y >= mapSize) return true;
+        return map[y][x] != DOT_EMPTY;
+    }
+
+    static boolean isMapFull() {
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkVictory(char symbol) {
+        for (int col = 0; col <= mapSize - winLength; col++) {
+            for (int row = 0; row < mapSize - winLength; row++) {
+                if (checkDiagonal(symbol, col, row) || checkLanes(symbol, col, row)) return true;
+            }
         }
         return false;
+    }
+
+    private static boolean checkDiagonal(char symbol, int offsetX, int offsetY) {
+        boolean toRight, toLeft;
+        toRight = true;
+        toLeft = true;
+
+        for (int i = 0; i < winLength; i++) {
+            toRight &= (map[i + offsetX][i + offsetY] == symbol);
+            toLeft &= (map[winLength - i - 1 + offsetX][i + offsetY] == symbol);
         }
 
+        return toRight || toLeft;
+    }
 
+    private static boolean checkLanes(char symbol, int offsetX, int offsetY) {
 
+        boolean cols, rows;
+        for (int col = offsetX; col < winLength + offsetX; col++) {
+            cols = true;
+            rows = true;
+            for (int row = offsetY; row < winLength + offsetY; row++) {
+                cols &= (map[col][row] == symbol);
+                rows &= (map[row][col] == symbol);
+            }
 
+            if (cols || rows) return true;
+        }
 
-
+        return false;
+    }
 }
